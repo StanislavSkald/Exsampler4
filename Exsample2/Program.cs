1b2f825aa5aafdd34640d856788ac71cf60dec23
Stanislsav <Sculkoskij@gmail.ru>
2023-03-13 16:39:18 +0300

    Задача 27

---
+++ Exsample2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Write(string maesageToUser)
+{
+    Console.WriteLine(maesageToUser);
+    return Convert.ToInt32 (Console.ReadLine());
+}
+
+int Sum (int N)
+{   
+    int Nt = 0;
+    while(N > 0)
+    {
+    Nt += N % 10;
+    N /= 10;
+    }
+    return(Nt);
+}
+
+int N = Write("Введите чисто");
+int res = Sum (N);
+Console.WriteLine(res);
---
+++ Exsample3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем\
+
+
+int [] arr()
+{
+    int [] array = new int [8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($" Введите число под индексом {i} ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+
+int[] array = arr();
+Console.WriteLine(string.Join(", ", array));
+
